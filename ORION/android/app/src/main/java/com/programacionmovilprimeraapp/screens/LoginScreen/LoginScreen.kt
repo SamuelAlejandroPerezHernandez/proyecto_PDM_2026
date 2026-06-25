4ea7fccc1dc99180fc1661228a309d6b2f671fac
SamuelAlejandroPerezHernandez <00052424@uca.edu.sv>
2026-06-25 16:29:13 -0600

    feat: agregar viewModel y UI para la pantalla de login

--- ORION/android/app/src/main/java/com/programacionmovilprimeraapp/screens/LoginScreen/LoginScreen.kt
+++ ORION/android/app/src/main/java/com/programacionmovilprimeraapp/screens/LoginScreen/LoginScreen.kt
@@ -1,3 +1,102 @@
 package com.programacionmovilprimeraapp.screens.LoginScreen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.lifecycle.viewmodel.compose.viewModel
 
+@Composable
+fun Login(
+    goToRegisterScreen: () -> Unit,
+    goToHome: () -> Unit
+){
+    val viewModel: LoginViewModel = viewModel()
+    val save by viewModel.saving.collectAsState()
+    val saveMessage by viewModel.savingMessage.collectAsState()
+    val userVerified by viewModel.userVerified.collectAsState()
+
+    val authUser = {
+            email: String, password: String ->
+        viewModel.LoginUser(email, password)
+    }
+
+    LaunchedEffect(userVerified){
+        if(userVerified == true){
+            goToHome()
+        }
+    }
+
+    LoginForm(
+        save,
+        saveMessage,
+        authUser,
+        goToRegisterScreen
+    )
+}
+
+@Composable
+fun LoginForm(
+    save: Boolean,
+    saveMessage: String?,
+    authUser: (String, String) -> Unit,
+    goToRegisterScreen: () -> Unit
+){
+
+    var emailText by rememberSaveable() { mutableStateOf("") }
+    var passwordText by rememberSaveable() { mutableStateOf("") }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        item {
+            OutlinedTextField(
+                value = emailText,
+                onValueChange = { emailText = it},
+                label = { Text(text = "Correo Elextronico") },
+                singleLine = true
+            )
+        }
+
+        item{
+            OutlinedTextField(
+                value = passwordText,
+                onValueChange = { passwordText = it},
+                label = { Text("Contraseña") },
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation()
+            )
+        }
+
+        item{
+            Button(
+                onClick = {
+                    authUser(emailText, passwordText)
+                }
+            ) {
+                Text(text = "Iniciar Sesion")
+            }
+        }
+
+        item {
+            Text( text = "no tienes una cuenta?")
+            Button(
+                onClick = { goToRegisterScreen() }
+            ) {
+                Text(text = "Registrarse")
+            }
+        }
+    }
+}
--- ORION/android/app/src/main/java/com/programacionmovilprimeraapp/screens/LoginScreen/LoginViewModel.kt
+++ ORION/android/app/src/main/java/com/programacionmovilprimeraapp/screens/LoginScreen/LoginViewModel.kt
@@ -1,1 +1,61 @@
 package com.programacionmovilprimeraapp.screens.LoginScreen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.programacionmovilprimeraapp.data.repository.AuthRepositoryImp
+import com.programacionmovilprimeraapp.domain.AuthRepository
+import com.programacionmovilprimeraapp.domain.LoginRequestModel
+import com.programacionmovilprimeraapp.orionnotes.MyApp
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class LoginViewModel(): ViewModel(){
+    private val Repository: AuthRepository = AuthRepositoryImp()
+
+    private val _saving = MutableStateFlow(false)
+    val saving = _saving.asStateFlow()
+
+    private val _savingMessage = MutableStateFlow<String?>(null)
+    val savingMessage = _savingMessage.asStateFlow()
+
+    private val emailRegex = "^[^\\s@]+@[^\\s@]+\\.[^\\s@]+\$".toRegex()
+
+    private val _userVerified = MutableStateFlow(false)
+    val userVerified = _userVerified.asStateFlow()
+
+    fun LoginUser(Email: String, Password: String){
+        if (Password.length < 6) {
+            _savingMessage.value = "La contraseña debe tener al menos 6 caracteres"
+            return
+        }
+
+        if (!emailRegex.matches(Email)) {
+            _savingMessage.value = "Por favor, introduce un correo electrónico válido"
+            return
+        }
+
+        viewModelScope.launch {
+            _saving.value = true
+
+            val loginPost = LoginRequestModel(
+                email = Email,
+                password = Password
+            )
+
+            Repository.LoginUser(loginPost)
+                .onSuccess {
+                        response ->
+                    MyApp.sessionManager.saveToken(response.token)
+                    _userVerified.value = true
+                    _savingMessage.value = "Usuario autenticado con exito"
+                }
+                .onFailure {
+                        e ->
+                    _savingMessage.value = "Error al autenticar el usuario: ${e.message}"
+                }
+
+            _saving.value = false
+        }
+    }
+}
