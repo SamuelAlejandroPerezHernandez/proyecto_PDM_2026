1d5c8553e197a2bfa6271e6045e1e061d743f231
SamuelAlejandroPerezHernandez <00052424@uca.edu.sv>
2026-06-25 18:45:17 -0600

    feat: agregar viewModel y UI de la pantalla de registro

--- ORION/android/app/src/main/java/com/programacionmovilprimeraapp/screens/RegisterScreen/RegisterViewModel.kt
+++ ORION/android/app/src/main/java/com/programacionmovilprimeraapp/screens/RegisterScreen/RegisterViewModel.kt
@@ -1,3 +1,51 @@
 package com.programacionmovilprimeraapp.screens.RegisterScreen
 
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.programacionmovilprimeraapp.data.repository.AuthRepositoryImp
+import com.programacionmovilprimeraapp.domain.AuthRepository
+import com.programacionmovilprimeraapp.domain.RegisterRequestModel
+import com.programacionmovilprimeraapp.orionnotes.MyApp
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class RegisterViewModel(): ViewModel(){
+
+    private val Repository: AuthRepository = AuthRepositoryImp()
+
+    private val _saving = MutableStateFlow(false)
+    val saving = _saving.asStateFlow()
+
+    private val _savingMessage = MutableStateFlow<String?>(null)
+    val savingMessage = _savingMessage.asStateFlow()
+
+    fun RegisterUser(Email: String, Password: String){
+        if (Password.length < 6) {
+            _savingMessage.value = "La contraseña debe tener al menos 6 caracteres"
+            return
+        }
+
+        viewModelScope.launch {
+            _saving.value = true
+
+            val registerPost = RegisterRequestModel(
+                email = Email,
+                password = Password
+            )
+
+            Repository.RegisterNewUser(registerPost)
+                .onSuccess {
+                        response ->
+                    _savingMessage.value = "Usuario registrado con exito"
+                }
+                .onFailure {
+                        e ->
+                    _savingMessage.value = "Error al crear el usuario: ${e.message}"
+                }
+
+            _saving.value = false
+        }
+    }
+}
